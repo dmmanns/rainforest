1b8cc19d8618ebc95c7123f540dd12f85c5b1103
dmmanns <dmmanns555@gmail.com>
2015-11-13 07:26:26 -0500

    Writing the review routes, controllers and views

--- rainforest/config/routes.rb
+++ rainforest/config/routes.rb
@@ -1,12 +1,22 @@
 Rainforest::Application.routes.draw do
+  get 'reviews/show'
+
+  get 'reviews/create'
+
+  get 'reviews/destroy'
+
   get 'sessions/new'
 
   get 'sessions/create'
 
   get 'sessions/destroy'
 
-  resources :products
+  resources :products 
   resources :sessions, only: [:new, :create, :destroy]
+
+  resources :products do
+    resources :reviews, only: [:show, :create, :destroy]
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
